01c3a1c990d091de98832e178726e75d85ebfa73
Zeeshan <zeeshan@taaruff.com>
2019-12-11 14:40:09 +0500

    Done

---
+++ app/Http/Controllers/TextFileController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Input;
+
+class TextFileController extends Controller
+{
+    //
+    public function index(){
+        return view('text');
+    }
+    public function testPost(Request $request){
+        if ($request->hasFile('file')) {
+            $file = $request->file->path();
+            $text = file_get_contents($file);
+            $myArray = [];
+            $lines = explode(PHP_EOL, $text);
+            $l = 0;
+            foreach($lines as $line) {
+                $myArray[$l] = explode("\t", $line);
+                $l++;
+            }
+            // return $myArray;
+            $path = $_SERVER['DOCUMENT_ROOT'] . "/text-files/";
+            if (!File::exists($path)) {
+                File::makeDirectory($path, $mode = 0775, true, true);
+            }
+            $myfile = $path . '/new-file.csv';
+            $fp = fopen($myfile, "wb");
+            $headers = array(
+                '*ContactName','EmailAddress','POAddressLine1','POAddressLine2','POAddressLine3','POAddressLine4','POCity','PORegion','POPostalCode','POCountry','*InvoiceNumber','Reference','*InvoiceDate','*DueDate','Total','InventoryItemCode','*Description','*Quantity','*UnitAmount','Discount','*AccountCode','*TaxType','TaxAmount','TrackingName1','TrackingOption1','TrackingName2','TrackingOption2','Currency','BrandingTheme'
+            );
+            fputcsv($fp, $headers);
+            $i = 0;
+            foreach ($myArray as $x) {
+                if(count($x) > 1){
+                    if($i > 0){
+                        $tmp_arr = array(
+                            $x[0], "", $x[1], $x[2], $x[3], $x[4], "", "", "", "", $x[5], $x[6], $x[7], $x[8], $x[9], $x[10], $x[11], $x[12], $x[13], $x[14], $x[16], $x[17], $x[18], "", "", "", "", "", ""
+                        );
+                        fputcsv($fp, $tmp_arr);
+                    }
+                    $i++;
+                }
+            }
+            fclose($fp);
+            $data = file_get_contents($myfile);
+            return $data;
+        }else{
+            return 'no file';
+        }
+    }
+}
